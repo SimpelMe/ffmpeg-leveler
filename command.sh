8f08eeda3f90e4d2af6f85c32235c41d19569dd7
Conrad Zelck <git@simpel.cc>
2019-10-21 00:40:19 +0200

    cleanup naming monitor points
    change abhoere to monitor
    
    Signed-off-by: Conrad Zelck <git@simpel.cc>

--- command.sh
+++ command.sh
@@ -29,21 +29,21 @@ ffmpeg -hide_banner $inputLoglevel \
     \
     -filter_complex "\
     [0:a:0] asetpts=N/SR/TB,\
-    dynaudnorm=$para_pa_leveler , asplit=2 [pa2ebu] [pa_dyn_abhoere] ;\
+    dynaudnorm=$para_pa_leveler , asplit=2 [pa2ebu] [pa_dyn_monitor] ;\
     [pa2ebu] ebur128=$para_ebur128 [vid_pa_ebu] [pa_ebu] ;\
     \
     [1:a:0] asetpts=N/SR/TB, \
-    compand=$para_trans_gate , asplit=2 [trans_gate_out] [trans_gate_abhoere] ;\
-    [trans_gate_out] compand=$para_trans_limiter , asplit=2 [trans_lim_out] [trans_lim_abhoere] ;\
-    [trans_lim_out] dynaudnorm=$para_trans_leveler , asplit=2 [trans2ebu] [trans_dyn_abhoere] ;\
+    compand=$para_trans_gate , asplit=2 [trans_gate_out] [trans_gate_monitor] ;\
+    [trans_gate_out] compand=$para_trans_limiter , asplit=2 [trans_lim_out] [trans_lim_monitor] ;\
+    [trans_lim_out] dynaudnorm=$para_trans_leveler , asplit=2 [trans2ebu] [trans_dyn_monitor] ;\
     [trans2ebu] ebur128=$para_ebur128 [vid_trans_ebu] [trans_ebu] ;\
     \
     [pa_ebu]volume=$para_mix_vol_pa[mix_int_1];\
     [trans_ebu]volume=$para_mix_vol_trans[mix_int_2];\
-    [mix_int_1][mix_int_2] amix=inputs=2:duration=longest, asplit=2 [mix_out][mix_abhoere] ;\
-    [mix_out]dynaudnorm=$para_mix_leveler, asplit=2 [dyn_out][mix_dyn_out] ;\
+    [mix_int_1][mix_int_2] amix=inputs=2:duration=longest, asplit=2 [mix_out][mix_monitor] ;\
+    [mix_out]dynaudnorm=$para_mix_leveler, asplit=2 [dyn_out][mix_dyn_monitor] ;\
     [dyn_out]loudnorm=$para_mix_loudnorm, \
-    ebur128=$para_ebur128 [vid_mix_ebu] [mix_loud_out] ;\
+    ebur128=$para_ebur128 [vid_mix_ebu] [mix_last_out_monitor] ;\
     \
     nullsrc=size=1280x960,fps=25,setpts=N/(25*TB) [vcanvas]; \
     testsrc=size=640x480[clock];\
@@ -53,8 +53,8 @@ ffmpeg -hide_banner $inputLoglevel \
     [tmp3][clock] overlay=shortest=1:x=640:y=480,setpts=PTS-STARTPTS [ebu_combined] " \
     \
     -map "[ebu_combined]" -c:v mpeg2video -g:0 10 -bf:0 0 -q:0 4 \
-    -map "[mix_loud_out]" -map "[mix_dyn_out]" -map "[mix_abhoere]" -map "[trans_dyn_abhoere]" \
-    -map "[trans_lim_abhoere]" -map "[trans_gate_abhoere]" -map "[pa_dyn_abhoere]" \
+    -map "[mix_last_out_monitor]" -map "[mix_dyn_monitor]" -map "[mix_monitor]" -map "[trans_dyn_monitor]" \
+    -map "[trans_lim_monitor]" -map "[trans_gate_monitor]" -map "[pa_dyn_monitor]" \
     -metadata:s:a:0 title="Output - gained to -16LUFS" \
     -metadata:s:a:1 title="Mix ducked" \
     -metadata:s:a:2 title="Mix unducked" \
